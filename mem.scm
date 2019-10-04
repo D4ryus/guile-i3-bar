355e42acac8ede9bd080c81488b2bc7a38aee4e9
d4ryus <d4ryus@teknik.io>
2019-10-04 22:11:09 +0200

    More memory info on click

--- mem.scm
+++ mem.scm
@@ -7,29 +7,41 @@
   #:use-module (oop goops))
 
 (define-class <mem> (<obj>)
-  total
-  used)
+  used
+  mem-total
+  mem-free
+  cached
+  active
+  inactive)
 
 (define-method (fetch (obj <mem>))
   (read-proc-meminfo!))
 
 (define-method (process (obj <mem>) (diff <number>))
-  (let* ((mem-info (slot-ref obj 'data))
-         (mem-total (get mem-info 'mem-total)))
-    (slot-set! obj 'total mem-total)
-    (slot-set! obj 'used (- mem-total (get mem-info 'mem-available)))
+  (let* ((mem-info (slot-ref obj 'data)))
+    (map (lambda (id)
+           (slot-set! obj id (ash (get mem-info id) -20)))
+         (list 'mem-free
+               'mem-total
+               'cached
+               'active
+               'inactive))
+    (slot-set! obj 'used (- (slot-ref obj 'mem-total)
+                            (ash (get mem-info 'mem-available) -20)))
     (list)))
 
 (define-method (fmt (obj <mem>) (clicked? <boolean>))
   (let ((mem-used (slot-ref obj 'used))
-        (mem-total (slot-ref obj 'total)))
+        (mem-total (slot-ref obj 'mem-total)))
     (values
-     (apply format #f
-            (if (> (/ mem-used mem-total) 0.9)
-                "<span foreground=\"#DA1000\">~4d~a</span>mb"
-                "~4d~amb")
-            (list (ash mem-used -20)
-                  (if clicked?
-                      (format #f "/~4d" (ash mem-total -20))
-                      "")))
+     (if clicked?
+         (apply format #f
+                "total: ~amb, free: ~amb, used: ~amb, cached: ~amb, active: ~amb, inactive: ~amb"
+                (map (lambda (slot) (slot-ref obj slot))
+                     '(mem-total mem-free used cached active inactive)))
+         (apply format #f
+                (if (> (/ mem-used mem-total) 0.9)
+                    "<span foreground=\"#DA1000\">~4d</span>mb"
+                    "~4dmb")
+                (list mem-used)))
      #:border (if clicked? "#777777" #f))))
